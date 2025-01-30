1015cd3d746e4ea01212c19559d7162b3da1ec10
Noud63 <noudvandun@gmail.com>
2025-01-30 11:22:42 +0100

    likebutton optimistic update swr

--- components/GetAllPosts.jsx
+++ components/GetAllPosts.jsx
@@ -3,17 +3,27 @@ import SinglePost from "./SinglePost";
 import Spinner from "./Spinner";
 import useSWR from "swr";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const GetAllPosts = () => {
 
   const { data, error, isLoading } = useSWR("/api/posts", fetcher);
+  
 
   if (error) return (
-    <div className="mx-auto mt-4 flex flex-col min-h-[200px] w-full max-w-[620px] items-center justify-center rounded-lg text-2xl text-white">
-      <Image src="/images/halloween.png" alt="error" width={150} height={150} />
+    <div className="mx-auto mt-8 flex min-h-[200px] w-full max-w-[620px] flex-col items-center justify-center rounded-lg text-2xl text-white">
+      <Image
+        src="/images/halloween.png"
+        alt="error"
+        width={145}
+        height={145}
+        sizes="100vw"
+        className="h-[145px] w-[145px]"
+      />
       <span>failed to load data!</span>
+      <span>Refresh page!</span>
     </div>
   );
   if (isLoading) return (
